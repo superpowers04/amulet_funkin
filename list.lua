ac3e6d8ef79aa262d4a39e424c049da147b54f21
Super <38338700+superpowers04@users.noreply.github.com>
2025-06-23 22:13:07 -0400

    Support asset folder mods

--- list.lua
+++ list.lua
@@ -17,20 +17,37 @@ do
 		if(line:find('%.json$')) then
 			jsons[line] = true
 		elseif line:find('Inst.ogg$') then
-			insts[line] = true
+			insts[line:sub(0,-9)] = true
 		end
 	end
-
+	local function exists(paths)
+		for i,v in pairs(paths) do
+			if(insts[v]) then return v end
+		end
+		return nil
+	end
 	for file in pairs(jsons) do
-		if(file:sub(-12) ~= "/events.json" and insts[file:gsub('[^/]+$','Inst.ogg')]) then
-			table.insert(songs,file)
+		if(file:sub(-12) ~= "/events.json") then
+			if(insts[file:gsub('[^/]+$','')]) then
+				table.insert(songs,{file})
+			else
+				local f = file:gsub('[^/]+$','')
+				local what = exists({
+					f:gsub('/data/','/songs/'),
+				})
+				if what then
+					table.insert(songs,{file,what})
+				end
+			end
 		end
 	end
-	table.sort(songs)
+	table.sort(songs,function(a,b) return a[1]<b[1] end)
 
 end
+if(#songs == 0) then return am.scale(2) ^ am.text('NO CHARTS!') end
 for index,song in pairs(songs) do
-	songList:append(am.translate(0,index*15) ^ am.text(song:gsub('^mods/',''):gsub('/charts/',' > '),nil,"left",'top'))
+	local song = song[1]
+	songList:append(am.translate(0,index*15) ^ am.text(song:gsub('/data/',' > '):gsub('^mods/',''):gsub('/charts/',' > '),nil,"left",'top'))
 end
 local hover,normal = vec4(1,1,1,1),vec4(0.6,0.6,0.6,1)
 
@@ -41,6 +58,10 @@ group:action(function(g)
 		local child = songList:child(i)
 		child.y = (scroll - i) * 15
 		child:child(1).color = i==scroll and hover or normal
+	end
+	local mw = win:mouse_wheel_delta().y
+	if(mw ~= 0) then
+		scroll = math.min(math.max(scroll-math.ceil(mw),1),#songs)
 	end
 	if(#win:keys_down() == 0) then
 		keyRepeat = 0
@@ -56,7 +77,7 @@ group:action(function(g)
 		keyRepeat = 0.2
 	end
 	if(win:key_pressed('enter')) then
-		win.scene = require('play')(songs[scroll])
+		win.scene = am.load_script('play.lua')()(unpack(songs[scroll]))
 	end
 end)
 
